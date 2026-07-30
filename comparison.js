a941715f278500b715c868cb1be02cad140e655c
Amalkrishna11 <a1malkrishna@gmail.com>
2026-07-30 08:08:43 +0530

    first commit

---
+++ comparison.js
@@ -0,0 +1,13 @@
+let a = 40 
+let b = 20 
+console.log(a==b);
+console.log(a===b);
+console.log(a>b);
+console.log(a>=b);
+console.log(a<b);
+console.log(a<=b);
+console.log(a!=b);
+console.log(a!==b);
+
+
+
---
+++ primitive.js
@@ -0,0 +1,188 @@
+// // var a=10
+// // console.log(typeof a);
+// // var b='am'
+// // console.log(typeof b);
+// //   let isLogin=true
+// //   console.log(typeof isLogin)
+
+// //   let c
+// //   console.log(typeof c);
+
+// //   let d=null
+// //   console.log(typeof d);
+// //   let num=273678682686482n
+// //   console.log(typeof num);
+
+// // let id=Symbol("id")
+// // console.log(typeof id);
+
+  
+  
+  
+// const user={
+//     name:"anu",
+//     age:"20"
+// }
+// console.log(user);
+
+// const number=[1,2,3,4]
+// console.log(number);
+// console.log(typeof number);
+
+// function greet()
+// {
+// console.log("hello");
+// }
+// greet()
+
+// function greet(name){
+//     console.log(name);
+    
+// }
+// greet("anu")
+
+// function gree(name){
+//     console.log("hii "+name);
+    
+// }
+// gree("anu")
+
+// function gre(name){
+//     console.log( `hi  ${name}`);
+    
+// }
+// gre("anu")
+// let name ="amal  ";
+// console.log(name.length);
+
+
+//slice
+// let str="try hard fail better"
+// console.log(str.slice(0,3));
+
+
+// let str1="mind ,power soul"
+// console.log(str1.substring(6,11));
+
+// let str2="mind, power, soul"
+// console.log(str2.substring(6,15));
+
+// let str3="power";
+// let new1=str3.replace("power,space")
+// console.log(new1);
+
+
+// let part="mind power power power soul"
+// let new part=part.replaceAll("power","space")
+
+
+// let message="hello worS";
+
+
+//29/7/26
+
+// let num=123;
+// console.log(typeof num);
+// let str=num.toString();
+// console.log(typeof str);
+
+
+
+// let pi=3.14159;
+// let newpi=pi.toFixed(2);
+// console.log(newpi);
+
+
+// let num1=5.9866
+// console.log(num1.toPrecision(4));
+
+// console.log(123.456.toExponential(2));
+
+// //value
+// let num2=new Number(123);
+// console.log(typeof num2);
+// let num3=num2.valueOf()
+// console.log(typeof num3);
+
+
+
+// //isfinite
+
+// console.log(Number.isFinite(123.456));
+// console.log(Number.isFinite(Infinity));
+
+
+// //isinteger
+
+// console.log(Number.isInteger(12.2233));
+// console.log(Number.isInteger(12));
+
+
+// //isnav
+
+// console.log(Number.isNaN(12));
+// console.log(Number.isNaN(NaN));
+
+
+// //parseint()
+// let num5=Number.parseInt("123");
+// console.log( typeof num5);
+
+
+// //persefloat
+
+// let num8=Number.parseFloat("123.345");
+// console.log(num6);
+// console.log(Number.parseFloat("123.456"));
+// console.log(typeof num6);
+
+
+let a=20,b=5;
+// console.log("sum",a+b);
+// console.log("dif",a-b);
+// console.log("product",a*b);
+// console.log("quotient",a/b);
+// console.log("reminder",a%b);
+
+
+
+// //a++
+
+// console.log(a++);
+// console.log(a);
+// console.log(++a);
+
+
+// //a--
+// console.log(b--);
+// console.log(b);
+// console.log(--b);
+
+
+
+//aszignment opertors
+
+let x=25;
+console.log(x+=10);  // x=x+10
+console.log(x-=10);   // x=x-10
+console.log(x*=10);    // x=x*10
+console.log(x/=10);     // x=x/10
+console.log(x%=10);     // x=x%10
+
+
+
+
+
+//comparison operators
+
+
+
+
+
+
+
+
+
+
+
+
