c8caeaa2c1a7004f2d44ac15173ca33b8f91ae69
Amalkrishna11 <a1malkrishna@gmail.com>
2026-07-31 08:52:41 +0530

    second commit

---
+++ forloop.js
@@ -0,0 +1,17 @@
+// for (let i=0;i<10;i++){
+//     console.log("hello");
+    
+// }
+
+
+
+// for (let a=1;a<=100;a++){
+//     if(a%2 == 0)
+//         console.log(a);
+        
+// }
+for (let a=1;a<=100;a++){
+    if(a%2 == 1)
+        console.log(a);
+        
+}
