af64e14ce1b84253175275600c1a970ef5cae8b3
Amalkrishna11 <a1malkrishna@gmail.com>
2026-08-05 08:21:21 +0530

    5_6_26class commit

---
+++ arrow.js
@@ -0,0 +1,23 @@
+// let a =()=>{
+//     console.log("hello");
+    
+
+// }
+// a()
+
+// let fun2=(a,b)=>{
+//     return a+b
+
+// }
+// console.log(fun2(10,30));
+
+
+// let add = (a,b)=> a+b
+// console.log(add(20,50));
+
+
+// let greeet=()=> "hello"
+// console.log(greeet());
+
+
+
