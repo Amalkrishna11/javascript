7e4db7a6e18f06c3d97bdf506b423a045412f5f7
Amalkrishna11 <a1malkrishna@gmail.com>
2026-08-05 08:44:44 +0530

    5_6_26class commit

--- iife.js
+++ iife.js
@@ -16,4 +16,4 @@
 // (function(name){
 //     console.log(name);
     
-// })("anu")
+// })("anu")                                                                                                                                                                                                                                                                                                         
